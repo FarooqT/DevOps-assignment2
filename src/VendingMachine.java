f1248f8b1f068a22ca0c41fbb512ba5eb3823501
Farooq <FarooqTayfoor@gmail.com>
2020-04-20 02:48:56 +0300

    VendingMachine code

---
+++ src/VendingMachine.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class VendingMachine {
+    HashMap <String, Double> items = new HashMap <String, Double>();
+    private String snackType;
+    private double price;
+
+    public void addItem(String item, double price){
+        items.put(item,price);
+    }
+    public void buyItem(String item, double money){
+        if(money< items.get(item))
+            throw new IllegalArgumentException();
+        items.remove(item);
+
+    }
+    public void printItems(){
+        items.forEach((key,value) -> System.out.println(key + " -> price: " + value));
+
+    }
+}
